ac05640741798b8ce9b2310b2d0dbce0d5a7392c
NoName0899 <lucas.matheus0899@gmail.com>
2019-09-25 22:29:28 -0300

    Adicionando Projetos

--- Atividade01/src/modelo/persistencia/ConexaoBD.java
+++ Atividade01/src/modelo/persistencia/ConexaoBD.java
@@ -19,19 +19,19 @@ public class ConexaoBD {
 
     public static Connection getConexao() throws DadosException {
 
-        try {
+        /* try {
             Class.forName("com.mysql.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://localhost/condominio", "root", "");
+            return DriverManager.getConnection("jdbc:mysql://localhost/cadastro", "root", "BDfox29@");
         } catch (ClassNotFoundException e) {
             throw new DadosException("Erro ao carregar JDBC: " + e.getMessage());
         } catch (SQLException e) {
             throw new DadosException("Erro ao conectar com SGBD: " + e.getMessage());
-        }
-        /*try {
+        }*/
+        try {
             if (conexao == null) {
-            Class.forName("com.mysql.jdbc.Driver");
-            conexao = DriverManager.getConnection("jdbc:mysql://localhost/cadastro", "root", "");
-        }
+                Class.forName("com.mysql.jdbc.Driver");
+                conexao = DriverManager.getConnection("jdbc:mysql://localhost/cadastro", "root", "BDfox29@");
+            }
         } catch (ClassNotFoundException ex) {
             throw new DadosException("Erro ao carregar JDBC!");
         } catch (SQLException ex) {
@@ -39,6 +39,5 @@ public class ConexaoBD {
         }
         return conexao;
 
-    }*/
     }
 }
---
+++ Atividade05/src/java/Controle/ExcluirAlunoServlet1.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.AlunoBO;
+import modelo.NegocioException;
+import modelo.entidades.Aluno;
+
+@WebServlet(name = "ListarAlunoServlet", urlPatterns = {"/aluno/excluir"})
+public class ExcluirAlunoServlet1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Aluno aluno = new Aluno();
+        int id = Integer.parseInt(request.getParameter("id"));
+        AlunoBO bo = new AlunoBO();
+        Aluno aluno = new Aluno();
+        aluno.setId(id);
+        
+        try {
+            bo.excluir(aluno);
+        } catch (NegocioException ex) {
+            throw new ServletException("", ex);
+        }
+        
+        response.sendRedirect(request.getContextPath()+"/aluno");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        throw new ServletException("Método não suportado");
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
